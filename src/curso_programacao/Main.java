ae20c00bf9db5e9ad7cd42f61f050b0eb7969231
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2023-06-03 19:55:55 -0300

    Adição de mais uma função (Média entre 3 números)

--- src/curso_programacao/Main.java
+++ src/curso_programacao/Main.java
@@ -17,22 +17,30 @@ public class Main {
 			System.out.println("2 - Média entre três números");
 			System.out.println("3 - Conversão de medidas");
 			System.out.println("4 - Encerrar");
-			System.out.println();
+			System.out.println(" ");
 			System.out.print("Sua opção: ");
+			System.out.println(" ");
 
 			Scanner scn = new Scanner(System.in);
 
 			if (scn.hasNextInt()) {
 				opcao = scn.nextInt();
-				scn.nextLine();
 
 				switch (opcao) {
 					case 1:
+						System.out.println(" ");
+						System.out.println("Iniciando . . .");
+						System.out.println(" ");
 						Media_dois_numeros mdn = new Media_dois_numeros();
 						mdn.calcularMedia();
+						continuar = false;
 						break;
 					case 2:
-						System.out.println("Indisponível");
+						System.out.println(" ");
+						System.out.println("Iniciando . . .");
+						System.out.println(" ");
+						Media_tres_numeros mtn = new Media_tres_numeros();
+						mtn.calcularMedia();
 						continuar = false;
 						break;
 					case 3:
@@ -52,7 +60,7 @@ public class Main {
 				System.out.println("Opção inválida. Tente novamente.");
 			}
 
-			System.out.println();
+			continuar = false;
 			scn.close();
 		}
 	}
--- src/curso_programacao/Media_dois_numeros.java
+++ src/curso_programacao/Media_dois_numeros.java
@@ -4,42 +4,35 @@ import java.util.Scanner;
 
 public class Media_dois_numeros {
 
-    public void calcularMedia() {
-        Double x1;
-        Double x2;
-        Double media;
+	public void calcularMedia() {
+		Double x1;
+		Double x2;
+		Double media;
 
-        Scanner scn = new Scanner(System.in);
+		Scanner scn = new Scanner(System.in);
 
-        System.out.println("Olá, mundo!");
-        System.out.println("Realizarei agora alguns testes de funcionalidades");
-        System.out.println("Me diga um número de 0 a 10:");
-        x1 = lerNumeroValido(scn);
-        if (x1 >= 0 && x1 <= 10) {
-            System.out.println("Agora me diga um segundo número de 0 a 10:");
-            x2 = lerNumeroValido(scn);
-            if (x2 >= 0 && x2 <= 10) {
-                System.out.println("Obrigado! A média entre os números citados é:");
-                media = (x1 + x2) / 2;
-                System.out.println(media);
-            } else {
-                System.out.println("Digite um número válido!");
-            }
-        } else {
-            System.out.println("Digite um número válido!");
-        }
+		System.out.println("Olá, mundo!");
+		System.out.println("Realizarei agora alguns testes de funcionalidades");
+		System.out.println("Me diga um número de 0 a 10:");
+		x1 = lerNumeroValido(scn);
+		System.out.println("Agora me diga um segundo número de 0 a 10:");
+		x2 = lerNumeroValido(scn);
+		System.out.println("Obrigado! A média entre os números citados é:");
+		media = (x1 + x2) / 2;
+		System.out.println(media);
+		
+		scn.close();
+	}
 
-        scn.close();
-    }
-
-    private Double lerNumeroValido(Scanner scanner) {
-        while (true) {
-            Double numero = scanner.nextDouble();
-            if (numero >= 0 && numero <= 10) {
-                return numero;
-            } else {
-                System.out.println("Digite um número válido!");
-            }
-        }
-    }
+	private Double lerNumeroValido(Scanner scanner) {
+		while (true) {
+			Double numero = scanner.nextDouble();
+			if (numero >= 0 && numero <= 10) {
+				return numero;
+			} else {
+				System.out.println("Digite um número válido!");
+				System.out.println(" ");
+			}
+		}
+	}
 }
