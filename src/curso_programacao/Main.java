f2b53daeb2fb137d916be72d7e3f2d29e7329f3b
MarcosBR315 <marcospaulo.pontes05@gmail.com>
2023-06-02 22:46:38 -0300

    Adicionada uma função de switch e uma classe nova

--- src/curso_programacao/Main.java
+++ src/curso_programacao/Main.java
@@ -5,44 +5,56 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Double x1;
-		Double x2;
-		Double media;
-		
-		Scanner scn = new Scanner(System.in);
-		
-		System.out.println("Olá, mundo!");
-		System.out.println("Realizarei agora alguns testes de funcionalidades");
-		System.out.println("Me diga um número de 0 a 10:");
-		x1 = lerNumeroValido(scn);
-		if(x1 >= 0 && x1 <= 10) {
-			System.out.println("Agora me diga um segundo número de 0 a 10:");
-			x2 = lerNumeroValido(scn);
-			if(x2 >= 0 && x2 <=10) {
-				System.out.println("Obrigado! A média entre os números citados é:");
-				media = (x1 + x2) / 2;
-				System.out.println(media);
+
+		int opcao;
+		boolean continuar = true;
+
+		System.out.println("Bem-vindo ao programa de funções!");
+
+		while (continuar) {
+			System.out.println("Qual das funções a seguir você deseja usar?");
+			System.out.println("1 - Média entre dois números");
+			System.out.println("2 - Média entre três números");
+			System.out.println("3 - Conversão de medidas");
+			System.out.println("4 - Encerrar");
+			System.out.println();
+			System.out.print("Sua opção: ");
+
+			Scanner scn = new Scanner(System.in);
+
+			if (scn.hasNextInt()) {
+				opcao = scn.nextInt();
+				scn.nextLine();
+
+				switch (opcao) {
+					case 1:
+						Media_dois_numeros mdn = new Media_dois_numeros();
+						mdn.calcularMedia();
+						break;
+					case 2:
+						System.out.println("Indisponível");
+						continuar = false;
+						break;
+					case 3:
+						System.out.println("Indisponível");
+						continuar = false;
+						break;
+					case 4:
+						System.out.println("Encerrando o programa...");
+						continuar = false;
+						break;
+					default:
+						System.out.println("Opção inválida.");
+						continuar = false;
+						break;
+				}
 			} else {
-				System.out.println("Digite um número válido!");
+				System.out.println("Opção inválida. Tente novamente.");
 			}
-		}else {
-			System.out.println("Digite um número válido!");
+
+			System.out.println();
+			scn.close();
 		}
-		
-		scn.close();   
-		
-	}
-	
-	private static Double lerNumeroValido(Scanner scanner) {
-        while (true) {
-            Double numero = scanner.nextDouble();
-            if (numero >= 0 && numero <= 10) {
-                return numero;
-            } else {
-                System.out.println("Digite um número válido!");
-            }
-        }
 	}
 
 }
